21147aaaac0361a207489bc91c15424b66694018
Xavi Vidal <systems3@letsbrico.com>
2021-12-13 17:47:09 +0100

    Added test frontend

---
+++ src/www/__test__/2021-12-13_upgrade_potato_store.spec.ts
@@ -0,0 +1,4 @@
+import { postLineSteps } from "./postLineSteps";
+import { PostTestFactory } from "./testPost/PostTestFactory";
+
+new PostTestFactory().create("2021-12-13_upgrade_potato_store", postLineSteps);
