c09a72bb00e48ff52de93896a084af7b0ad27e04
Xavi Vidal <systems3@letsbrico.com>
2021-12-13 21:58:01 +0100

    Added test frontend

---
+++ src/www/__test__/2021-12-13_patata_emoji.spec.ts
@@ -0,0 +1,4 @@
+import { postLineSteps } from "./postLineSteps";
+import { PostTestFactory } from "./testPost/PostTestFactory";
+
+new PostTestFactory().create("2021-12-13_patata_emoji", postLineSteps);
